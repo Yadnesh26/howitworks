d3d2615f25cf227abe9808dabc0daabb0a39d116
Yadnesh26 <decoderop@gmail.com>
2026-07-22 22:14:47 +0530

    Remove binary search explainer files

--- src/explainers/binary-search/meta.js
+++
@@ -1,7 +0,0 @@
-export default {
-  id: 'binary-search',
-  title: 'Binary Search',
-  summary: 'How computer science finds a needle in a haystack.',
-  accent: '#00e676',
-  categories: ['computers'],
-};
